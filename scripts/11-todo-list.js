221e2a2b03062ba0583902eb1fd220a95e8a26ac
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-12 16:00:47 +0100

    📜 Created the to-do list project in lesson `11`.

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,29 +1,67 @@
-const toDoListArray = [];
+const toDoList1 = [];
 
-const handleButtonClick = () => {
-	// IMPORTANT: For input elements, it's `.value`.
-	const item = document.querySelector("input.js-todo-name-input");
-
-	// @ts-ignore
-	toDoListArray.push(item.value);
+const handleButtonClick1 = () => {
+	const item = document.querySelector("input.js-item-input-1");
 
 	// @ts-ignore
-	item.value = "";
-	console.log(toDoListArray);
+	if (item.value) {
+		// @ts-ignore
+		toDoList1.push(item.value);
+
+		console.log(toDoList1);
+
+		// @ts-ignore
+		item.value = "";
+	}
 };
 
-const toDoItems = document.querySelector("div.js-to-do-items");
-
 const handleButtonClick2 = () => {
-	const item = document.querySelector("input.js-todo-name-input-2");
+	const item = document.querySelector("input.js-item-input-2");
 
 	// @ts-ignore
-	toDoListArray.push(item.value);
+	if (item.value) {
+		const itemsDiv = document.querySelector("div.js-items-2");
 
-	// "Generating" the HTML.
-	// @ts-ignore
-	toDoItems.innerHTML += `<p>${item.value}</p>`;
+		// @ts-ignore
+		itemsDiv.innerHTML += `<p>${item.value}</p>`;
+
+		// @ts-ignore
+		item.value = "";
+	}
+};
+
+const toDoList3 = [];
+const itemsDiv = document.querySelector("div.js-items-3");
+
+const renderToDoList = () => {
+	itemsDiv.innerHTML = "";
+
+	toDoList3.forEach((item, index) => {
+		console.log(itemsDiv.innerHTML, index, toDoList3);
+
+		itemsDiv.innerHTML += `
+		<div class="to-do-row">
+			<div class="item-name">${item.item}</div>
+			<div class="item-date">${item.date}</div>
+			<button onclick="toDoList3.splice(${index}, 1);renderToDoList();" class="delete-button">Delete</button>
+		</div>`;
+	});
+};
+
+const handleButtonClick3 = () => {
+	const item = document.querySelector("input.js-item-input-3");
+	const date = document.querySelector("input.js-date-input-3");
 
 	// @ts-ignore
-	item.value = "";
+	if (item.value && date.value) {
+		// @ts-ignore
+		toDoList3.push({item: item.value, date: date.value});
+
+		renderToDoList();
+
+		// @ts-ignore
+		item.value = "";
+		// @ts-ignore
+		date.value = "";
+	}
 };
