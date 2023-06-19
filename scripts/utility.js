e1bfd950feb9da5d7bbeea43cca63489392b2cff
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-19 15:09:31 +0100

    🛒 Moved the `updateCart` function to a new `utility.js` file.

---
+++ scripts/utility.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+/* eslint-disable no-undef */
+/* eslint-disable func-style */
+
+const cartQuantity = document.querySelector("div.cart-quantity");
+
+function updateCart() {
+	localStorage.setItem("cart", JSON.stringify(cart));
+
+	let total = 0;
+	for (const item in cart) {
+		total += cart[item].quantity;
+	}
+
+	if (cartQuantity) cartQuantity.innerHTML = `${total}`;
+}
